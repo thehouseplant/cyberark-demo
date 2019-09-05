d173c67b365fb4664c7ffd1a162a25e9bb551dc0
thehouseplant <sean.collins@outlook.com>
2019-09-05 15:55:10 -0700

    Add limit to while loop for building the image

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-while true; do
+x=1
+while [ $x -le 30 ]
+do
     echo "Running CyberArk demo application for 5s..."
     go run main.go
     sleep 5
+    x=$(( $x + 1 ))
     echo "Killing application"
 done
