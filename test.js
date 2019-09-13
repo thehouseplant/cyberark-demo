d6ad0990e7bc90782a2d35c6f79aff1dfff5a467
sean0000 <scollins@esri.com>
2019-09-13 15:46:58 -0700

    Add token test

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
 // Test config without stuff
+const AWS_KEY = 'AKIAJCK2WUHJ2653GNBQ'
+const awsAccessId = 'AKIAJCK2WUHJ2653GNBQ'
+const AWS = 'AKIAJCK2WUHJ2653GNB'
+
+const accessToken = '25f94a2a5c7fbaf499c665bc73d67c1c87e496da'
+const PAT = '25f94a2a5c7fbaf499c665bc73d67c1c87e496da'
+const token = '25f94a2a5c7fbaf499c665bc73d67c1c87e496da'
